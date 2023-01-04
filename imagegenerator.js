413f90adfdb03acd0f5fce68a5778a5628223c67
agboagabah <agboagabaa@gmail.com>
2023-01-03 18:40:28 -0800

    Most recent update

--- imagegenerator.js
+++ imagegenerator.js
@@ -1,9 +1,24 @@
-const r_image = [
+const pics = [ 'url("https://images.pexels.com/photos/6900713/pexels-photo-6900713.jpeg?auto=compress&cs=tinysrgb&w=600")',
+ 'url ("https://images.pexels.com/photos/7676308/pexels-photo-7676308.jpeg?auto=compress&cs=tinysrgb&w=600")' ,
+ 'url ("https://images.pexels.com/photos/5740932/pexels-photo-5740932.jpeg?auto=compress&cs=tinysrgb&w=600")',
+'url("https://images.pexels.com/photos/6063469/pexels-photo-6063469.jpeg?auto=compress&cs=tinysrgb&w=600")',
+'url ("https://images.pexels.com/photos/4596850/pexels-photo-4596850.jpeg?auto=compress&cs=tinysrgb&w=600")', 
+'url("https://images.pexels.com/photos/1546863/nature-milky-way-galaxy-constellations-1546863.jpeg?auto=compress&cs=tinysrgb&w=600")',
+'url("https://images.pexels.com/photos/6109101/pexels-photo-6109101.jpeg?auto=compress&cs=tinysrgb&w=600")',
+'url("https://images.pexels.com/photos/6943131/pexels-photo-6943131.jpeg?auto=compress&cs=tinysrgb&w=600")' ,
+'url("https://images.pexels.com/photos/7723324/pexels-photo-7723324.jpeg?auto=compress&cs=tinysrgb&w=600")' ,
+'url("https://images.pexels.com/photos/8152002/pexels-photo-8152002.jpeg?auto=compress&cs=tinysrgb&w=600")'];
 
-],
 
-rm = document.getElementById('my_image'),
-rand_int = Math.floor(Math.random() *10);
 
-rm.src = r_image[rand_int];
+const pic = document.querySelector('section');
 
+function showImage() {
+    var a = Math.floor(Math.random() * pics.length);
+    var img = pics[a];
+    pic.style.backgroundImage = img;
+}
+
+setInterval(showImage, 1500);
+
+ 
--- shortquiz.js
+++ shortquiz.js
@@ -1,223 +1,139 @@
-const restartBtn = document.getElementById('restart');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const submitBtn = document.getElementById('submit');
-const trueBtn = document.getElementById('True');
-const falseBtn = document.getElementById('False');
-const userScore = document.getElementById('user-score');
-const questionText= document.getElementById('question-text');
-
-let currentQuestion = 0;
-let score = 0;
-let questions = [
-    {
-        question: 'Barrack Obama is the current President of the United State',
-        answers: [
-            { option: 'For sure!', answer: true
-
-            },
-            {
-                option: 'no', answer: false
-            }
-        ]
-    },
-
- { question: 'All humans must die',
-    answers:[
-        { option: 'Yes', answer: true
-
-        },
-        {
-            option: 'I guess no', answer: false
-        }
+   
+const Questions = [{
+    id: 0,
+    q: "What is capital of India?",
+    a: [{ text: "gandhinagar", isCorrect: false },
+        { text: "Surat", isCorrect: false },
+        { text: "Delhi", isCorrect: true },
+        { text: "mumbai", isCorrect: false }
     ]
 
- },
- 
- 
- { question: 'The sun shines during the night',
-    answers:[
-        {option: 'Noo', answer: 'true'
-
-        },
-        {
-            option:'yes' , answer: 'false'
-        }
-    ]
-
-
- },
- 
- { question: 'You can eat water' ,
-    answers:[
-        {option:'yes' , answer: 'false'
-
-        },
-        {
-            option: 'no', answer: 'true'
-        }
-    ]
-    },
-    
-    { question: 'Javascript is easy to learn',
-        answers:[
-            {option: 'yes' , answer:'false'
-    
-            },
-            {
-                option:'no' , answer: 'true'
-            }
-        ]
-    },
-
-    { question: 'This quiz is easy',
-    answers:[
-        {option: 'yes' , answer: 'true'
-
-        },
-        {
-            option: 'no' , answer: 'false'
-        }
-    ]
 },
+{
+    id: 1,
+    q: "What is the capital of Thailand?",
+    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+        { text: "phuket", isCorrect: false },
+        { text: "Ayutthaya", isCorrect: false },
+        { text: "Bangkok", isCorrect: true }
+    ]
 
-];
+},
+{
+    id: 2,
+    q: "Will all men die",
+    a: [{ text: "No", isCorrect: false },
+        { text: "Certainly not", isCorrect: false },
+        { text: "Yes", isCorrect: true },
+        { text: "Maybe", isCorrect: false }
+    ]
 
-restartBtn.addEventListener('click' , restart);
-prevBtn.addEventListener('click' , prev);
-nextBtn.addEventListener('click' , next);
-submitBtn.addEventListener('click', submit);
+} 
 
-function  beginQuiz() {
-    currentQuestion = 0;
-    questionText.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML = questions[currentQuestion].answer[0].option;
-    trueBtn.onclick = () => {
-        let ano = 0;
-        if (questions[currentQuestion].answers[ano].answer) { 
-            if (score<5) {
-                score++;
-            }
-            
-        }
+]
+
+
+var start = true;
+
+
+function iterate(id) {
+
+
+var result = document.getElementsByClassName("result");
+result[0].innerText = "";
+
+
+const question = document.getElementById("question");
+
+
+
+question.innerText = Questions[id].q;
+
+const op1 = document.getElementById('op1');
+const op2 = document.getElementById('op2');
+const op3 = document.getElementById('op3');
+const op4 = document.getElementById('op4');
+
+
+
+op1.innerText = Questions[id].a[0].text;
+op2.innerText = Questions[id].a[1].text;
+op3.innerText = Questions[id].a[2].text;
+op4.innerText = Questions[id].a[3].text;
+
+
+op1.value = Questions[id].a[0].isCorrect;
+op2.value = Questions[id].a[1].isCorrect;
+op3.value = Questions[id].a[2].isCorrect;
+op4.value = Questions[id].a[3].isCorrect;
+
+var selected = "";
+
+
+op1.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightgoldenrodyellow";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op1.value;
+})
+
+ 
+op2.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightgoldenrodyellow";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op2.value;
+})
+
+ 
+op3.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightgoldenrodyellow";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op3.value;
+})
+
+ 
+op4.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightgoldenrodyellow";
+    selected = op4.value;
+})
+
+ 
+const evaluate = document.getElementsByClassName("evaluate");
+
+
+evaluate[0].addEventListener("click", () => {
+    if (selected == "true") {
+        result[0].innerHTML = "True";
+        result[0].style.color = "green";
+    } else {
+        result[0].innerHTML = "False";
+        result[0].style.color = "red";
     }
-    userScore.innerHTML = score;
-    if(currentQuestion <5) {
-        next();
-    }
-
-
-falseBtn.innerHTML = questions[currentQuestion].answer[1].option;
-falseBtn.onclick = () => {
-    let anon = 1;
-    if(questions[currentQuestion].answers[anon].answer)
-    {
-        if(score<5){
-            score++; 
-        }
-    }
-    userScore.innerHTML = score;
-    if(currentQuestion<2){
-        next();
-    }
-}
-prevBtn.classList.add('hide');
-}
-beginQuiz();
-
-function restart( ) {
-    currentQuestion =0;
-    prevBtn.classList.remove('hide');
-    nextBtn.classList.remove('hide');
-    submitBtn.classList.remove('hide');
-    trueBtn.classList.remove('hide');
-    falseBtn.classList.remove('hide');
-    score = 0;
-    userScore.innerHTML = score;
-    beginQuiz();
+})
 }
 
-function next() {
-    currentQuestion++;
-    if (currentQuestion>=2){
-nextBtn.classList.add('hide');
-prevBtn.classList.remove('hide');  
-}
-questionText.innerHTML = questions[currentQuestion].question;
-trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-trueBtn.onclick = () => {
-    let anon = 0;
-    if(questions[currentQuestion].answers[anon].answer){
-        if (score<4){
-            score++;
-        
-        }
-    }
-    userScore.innerHTML = score;
-    if(currentQuestion<2){
-        next ();
-    } 
-}
-falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-falseBtn.onclick = () =>{
-    let ano =1;
-    if (questions[currentQuestion].answers[ano].answer){
-        if(score<4){
-            score++;
-
-    }
-}
- userScore.innerHTML = score;
- if (currentQuestion<3){
-    next();
- }
-
-}
-prevBtn.classList.remove('hide');
+if (start) {
+iterate("0");
 }
 
-function prev( ) { currentQuestion--;
-    if (currentQuestion<=0){
-        prevBtn.classList.add('hide');
-        nextBtn.classList.remove('hide');
-    }
-    questionText.innerHTML = questions[currentQuestion].question;
-    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
-    trueBtn.onclick = () => {
-        let ano =0;
-        if(questions[currentQuestion].answers[ano].answer){
-            if (score <4){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQuestion<3) {
-            next() ;
-        }
-    }
-    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
-    falseBtn.onclick = ()=>{
-        let ano = 1;
-        if(questions[currentQuestion].amswers[ano].answer){
-            if (score<4){
-                score++;
-            }
-        }
-        userScore.innerHTML=score;
-        if
-            (currentQuestion<3){
-                next();
-            }
-        
-    }
-    nextBtn.classList.remove('hide');
+ 
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 2) {
+    id++;
+    iterate(id);
+    console.log(id);
 }
 
-function submit( ) {
-    prevBtn.classList.add('hide');
-    nextBtn.classList.add('hide');
-    submitBtn.classList.add('hide');
-    trueBtn.classList.add('hide');
-    falseBtn.classList.add('hide');
-    questionText.innerHTML = 'Congratulations on submiting the quiz!';
-}
+})
